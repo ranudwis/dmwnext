5f3d58c715154ada783bdff4f293f0c18397fd76
Ranu Dwi Setiadi <ranudwis@gmail.com>
2019-10-02 23:27:51 +0700

    Create landingpage header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,60 @@
+@extends('partial/main')
+
+@section('content')
+    @component('partial/header')
+        @slot('class')
+            is-primary is-medium is-bold
+        @endslot
+
+        <div class="container has-text-centered">
+            <h2 class="title is-1">DMW++</h2>
+            <p class="subtitle is-3">
+                Database Modul dan Webtutor
+            </p>
+
+            <div class="field">
+                <div class="control has-icons-right">
+                    <input class="input is-large is-rounded" type="text" placeholder="Cari Mata Kuliah..." autofocus />
+
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-search"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        @slot('footer')
+            <div class="container mainfeaturelink">
+                <div class="buttons">
+                    <a href="" class="button is-medium is-white is-outlined">
+                        <span class="icon">
+                            <i class="fas fa-book fa-fw"></i>
+                        </span>
+
+                        <span>Mata Kuliah</span>
+                    </a>
+                    <a href="" class="button is-medium is-white is-outlined">
+                        <span class="icon">
+                            <i class="fas fa-file-alt"></i>
+                        </span>
+
+                        <span>Webtutor</span>
+                    </a>
+                </div>
+            </div>
+        @endslot
+    @endcomponent
+
+    <section class="section">
+        <div class="container">
+            <h3 class="title has-text-centered">Apa itu DMW++?</h3>
+
+            <div class="content">
+                <p>
+                    DMW++ adalah website yang disediakan oleh Divisi Pendidikan Dan Pelatihan
+                    Himpunan Mahasiswa Informatika Universitas Diponegoro
+                </p>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partial/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>DMW++</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="{{ asset('fonts/fontawesome-5.9.0/css/all.css') }}" />
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
