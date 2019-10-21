3ad5b8df7b2da66b8c625ccc355dd94365b75cef
Ranu Dwi Setiadi <ranudwis@gmail.com>
2019-10-21 14:34:01 +0700

    Remanaging color in sass to add more color and change primary color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,45 @@
 @section('content')
     @component('partial/header')
         @slot('class')
-            is-primary is-medium is-bold
+            is-medium is-bold is-indigo
+        @endslot
+
+        @slot('header')
+            <div class="hero-head">
+                <nav class="navbar" role="navigation">
+                    <div class="container">
+                        <div class="navbar-brand">
+                            <a class="navbar-item" href="">
+                                <img src="{{ asset('images/logo.jpg') }}" />
+                                DMW++
+                            </a>
+
+                            <a class="navbar-burger burger" data-target="mainNavbar">
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </div>
+
+                        <div id="mainNavbar" class="navbar-menu">
+                            <div class="navbar-start">
+                                <a class="navbar-item" href="">Mata Kuliah</a>
+                                <a class="navbar-item" href="">Web Tutorial</a>
+                                <a class="navbar-item" href="">ASK</a>
+                            </div>
+
+                            <div class="navbar-end">
+                                <div class="navbar-item">
+                                    <div class="buttons">
+                                        <a class="button is-white" href=" {{ route('auth.signin') }}">Masuk</a>
+                                        <a class="button is-white is-outlined" href="{{ route('auth.signup') }}">Daftar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         @endslot
 
         <div class="container has-text-centered">
--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -1,39 +1,5 @@
 <div class="hero {{ $class }}">
-    <div class="hero-head">
-        <nav class="navbar" role="navigation">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="navbar-item" href="">
-                        <img src="{{ asset('images/logo.jpg') }}" />
-                        DMW++
-                    </a>
-
-                    <a class="navbar-burger burger" data-target="mainNavbar">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="mainNavbar" class="navbar-menu">
-                    <div class="navbar-start">
-                        <a class="navbar-item" href="">Mata Kuliah</a>
-                        <a class="navbar-item" href="">Web Tutorial</a>
-                        <a class="navbar-item" href="">ASK</a>
-                    </div>
-
-                    <div class="navbar-end">
-                        <div class="navbar-item">
-                            <div class="buttons">
-                                <a class="button is-white" href="">Masuk</a>
-                                <a class="button is-white is-outlined" href="{{ route('auth.signup') }}">Daftar</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </div>
+    {{ $header ?? null }}
 
     <div class="hero-body">
         {{ $slot }}
