b2b4e9fded0520add6424f9dbc4825a33ea6c466
Ranu Dwi Setiadi <ranudwis@gmail.com>
2019-10-23 10:07:19 +0700

    Add AdminAccessMiddleware

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         $this->attributes['password'] = bcrypt($value);
     }
+
+    public function isAdmin()
+    {
+        return $this->admin;
+    }
 }
